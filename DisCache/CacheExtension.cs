a6c0c512b9f327f37cefb37ac677ab7f5b667684
Ngoc Nguyen <nnguyen@corp.ipipenet.com>
2018-04-23 16:23:16 -0400

    Add more functionalities: Get keys & Clear cache

--- DisCache/CacheExtension.cs
+++ DisCache/CacheExtension.cs
@@ -1,17 +1,30 @@
 ﻿using Microsoft.Extensions.Caching.Distributed;
 using Newtonsoft.Json;
+using System.Collections.Generic;
 using System.Threading.Tasks;
 
 namespace DisCache
 {
+    // Extension methods for cache
     public static class CacheExtension
     {
+        private static List<string> keys = new List<string>();  // keys added to cache
 
+        // Set cache with specific key, store value as a JSON
         public static async Task SetCacheAsync<T>(this IDistributedCache cache, string key, T val)
         {
+            await AddKeyAsync(key);
             await cache.SetStringAsync(key, JsonConvert.SerializeObject(val));
         }
 
+        // Add a new key-value cache entry to a specific group
+        public static async Task AddCacheToGroupAsync<T>(this IDistributedCache cache, string group, string key, T val)
+        {
+            await AddKeyToGroupAsync(group, key);
+            await cache.SetCacheAsync(group + "_" + key, val);
+        }
+
+        // Get cache value from a specific key, return an object
         public static async Task<T> GetCacheAsync<T>(this IDistributedCache cache, string key)
         {
             var val = await cache.GetStringAsync(key);
@@ -20,9 +33,77 @@ namespace DisCache
             return JsonConvert.DeserializeObject<T>(val);
         }
 
+        // Get all keys from a specif group, return a list of strings
+        public static async Task<List<string>> GetKeyGroupAsync(this IDistributedCache cache, string group)
+        {
+            List<string> keyList = new List<string>();
+            await Task.Run(() =>
+            {
+                foreach (string key in keys)
+                {
+                    if (key.Contains(group))
+                        keyList.Add(key);
+                }
+            });
+            return keyList;
+        }
+
+        // Get all keys, return a list of strings
+        public static async Task<List<string>> GetAllKeysAsync(this IDistributedCache cache)
+        {
+            return await GetKeyListAsync();
+        }
+
+        // Clear cache data by key
         public static async Task RemoveCacheAsync(this IDistributedCache cache, string key)
         {
-            await cache.RemoveCacheAsync(key);
+            await cache.RemoveAsync(key);
         }
+
+        // Clear all cache by group
+        public static async Task RemoveCacheByGroupAsync(this IDistributedCache cache, string group)
+        {
+            List<string> keyList = await cache.GetKeyGroupAsync(group);
+            foreach (string key in keyList)
+            {
+                await cache.RemoveCacheAsync(key);
+            }
+        }
+
+        // Helper methods
+
+        private static async Task AddKeyAsync(string key)
+        {
+            await Task.Run(() => 
+            {
+                if (!keys.Contains(key)){
+                    keys.Add(key);
+                }
+            });
+        }
+
+        private static void AddKey(string key)
+        {
+            if (!keys.Contains(key))
+            {
+                keys.Add(key);
+            }
+        }
+        private static async Task AddKeyToGroupAsync(string group, string key)
+        {
+            await AddKeyAsync(group + "_" + key);
+        }
+
+        private static void AddKeyToGroup(string group, string key)
+        {
+            AddKey(group + "_" + key);
+        }
+
+        private static async Task<List<string>> GetKeyListAsync()
+        {
+            return await Task.Run(() => keys);
+        }
+
+        
     }
 }
--- DisCache/UseMiddlewareExtension.cs
+++ DisCache/UseMiddlewareExtension.cs
@@ -2,6 +2,7 @@
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Caching.Distributed;
 using System.Threading.Tasks;
+using System.Collections.Generic;
 
 namespace DisCache
 {
@@ -31,7 +32,12 @@ namespace DisCache
 
         public async Task Invoke(HttpContext context)
         {
-            await cache.SetCacheAsync("CurrentUser", new User{Username = "2BisBestGrill", Email = "2BFanSub@nier.com"});
+            await cache.AddCacheToGroupAsync("YorHa", "2B", new User{Username = "2BisBestGrill", Email = "2BFanSub@nier.com"});
+            await cache.AddCacheToGroupAsync("YorHa", "9S", new User { Username = "9Sx2B4ever", Email = "9Sx2B@nier.com" });
+            await cache.AddCacheToGroupAsync("YorHa", "6O", new User { Username = "Operator6O", Email = "LunarTear@nier.com" });
+            await cache.AddCacheToGroupAsync("Rogue", "A2", new User { Username = "A2Kills2B", Email = "A2IsMyWaifu@nier.com" });
+            await cache.AddCacheToGroupAsync("Rogue", "Emil", new User { Username = "Determination", Email = "BigHead@nier.com" });
+            await cache.AddCacheToGroupAsync("God", "YokoTaro", new User { Username = "ShitSquareEnix", Email = "YokoTaro@nier.com" });
             await this.next(context);
         }
     }
@@ -49,8 +55,36 @@ namespace DisCache
 
         public async Task Invoke(HttpContext context)
         {
-            var user = await cache.GetCacheAsync<User>("CurrentUser");
+            /*
+            var user = await cache.GetCacheAsync<User>("YorHa_2B");
             await context.Response.WriteAsync($"Username: {user.Username} --- Email: {user.Email}");
+            */
+            List<string> yorha = await cache.GetKeyGroupAsync("YorHa");
+            List<string> rogue = await cache.GetKeyGroupAsync("Rogue");
+            List<string> god = await cache.GetKeyGroupAsync("God");
+            await context.Response.WriteAsync("YorHa group:\n");
+            foreach (string key in yorha)
+            {
+                await context.Response.WriteAsync($"key: {key}\n");
+                var user = await cache.GetCacheAsync<User>(key);
+                await context.Response.WriteAsync($"Username: {user.Username} --- Email: {user.Email}\n");
+            }
+            await context.Response.WriteAsync("Rogue group:\n");
+            foreach (string key in rogue)
+            {
+                await context.Response.WriteAsync($"key: {key}\n");
+                var user = await cache.GetCacheAsync<User>(key);
+                await context.Response.WriteAsync($"Username: {user.Username} --- Email: {user.Email}\n");
+            }
+            await context.Response.WriteAsync("God group:\n");
+            
+            foreach (string key in god)
+            {
+                await context.Response.WriteAsync($"key: {key}\n");
+                var user = await cache.GetCacheAsync<User>(key);
+                await context.Response.WriteAsync($"Username: {user.Username} --- Email: {user.Email}\n");
+            }
+            await cache.RemoveCacheByGroupAsync("God");
         }
     }
 }
